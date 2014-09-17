5aabb8cc0d97a7e589122fe714b51cb1754925d3
julioneto <julioneto20@hotmail.com>
2014-09-17 00:05:00 -0300

    alteração

--- Teste-Git/src/Teste.java
+++ Teste-Git/src/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("opa hehe :) :p");
 		
-		System.out.println("novo teste para comitar");
+		System.out.println("novo teste para comitar asdf asdfasdfa");
 	}
 
 }
